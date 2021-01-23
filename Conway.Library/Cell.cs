313b8bc3704d007f960e96b34fba93cb89882894
@vic10us <dev@vic10.us>
2021-01-23 16:43:37 -0500

    Add project files.

---
+++ Conway.Library/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Conway.Library
+{
+    public class Cell
+    {
+        public CellState State;
+        public CellState Next;
+        public readonly List<Cell> Neighbors = new List<Cell>();
+        public int LiveNeighbors => Neighbors.Count(x => x.State.Equals(CellState.Alive));
+        public CellState NextState => LifeRules.GetNewState(State, LiveNeighbors);
+    }
+}
